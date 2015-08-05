071f5f63c4fb9024b500ca8b9e06725a18fa35f3
Martin Herrmann <martin@famherrmann.de>
2015-08-06 01:54:08 +0200

    Experimental elements

--- src/element.py
+++ src/element.py
@@ -3,12 +3,52 @@ from collections import namedtuple
 Style = namedtuple("Style", ["name", "inherit"]) 
 
 class Element:
-    def __init__(self, parent, objectClasses = []):
-        self.parent = parent
-        self.objectClasses = []
+    def __init__(self, classes = None):
+        self.classes = classes
         
     def children(self):
         raise NotImplementedError()
     
     def styles(self):
         raise NotImplementedError()
+
+    def dump(self, indentString = "  ", indentLevel = 0):
+        indentPrefix = indentString * indentLevel
+        print(indentPrefix + str(self.__class__.__name__) + " " + str(self.classes))
+        
+        for child in self.children():
+            child.dump(indentString, indentLevel + 1)
+        
+
+
+class Chart(Element):
+    def __init__(self, classes = None):
+        super(Chart, self).__init__(classes)
+        self.serien = []
+        
+    def children(self):
+        return self.serien
+        
+    def add_series(self, series):
+        self.serien.append(series)
+        
+    def create_series(self, *args, **kwargs):
+        self.add_series(Series(*args, **kwargs))
+
+class Series(Element):
+    def __init__(self, classes = None, x = None, y = None):
+        super(Series, self).__init__(classes)
+        self.points = [Point(None, px, py) for px, py in zip(x, y)]
+        
+    def children(self):
+        return self.points
+
+class Point(Element):
+    def __init__(self, classes = None, x = None, y = None):
+        super(Point, self).__init__(classes)
+        self.x = x
+        self.y = y
+
+    def children(self):
+        return []
+    
--- src/test.py
+++ src/test.py
@@ -1,5 +1,11 @@
 from element import *
 
+chart = Chart()
+chart.add_series(Series(["even"], x = [-2, -1, 0, 1, 2, 3], y = [4,1,0,1,4,9]))
+chart.add_series(Series(["odd" ], x = [-1, 0, 1, 2]       , y = [-1, 0, 1, 8]))
+
+chart.dump()
+
 # Create a chart with some series
 
 # Set the styles directly
